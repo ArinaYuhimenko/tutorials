005a996a768f7817fb1fd1d931e9429e28be321d
ArinaYuhimenko <115879035+ArinaYuhimenko@users.noreply.github.com>
2023-05-23 22:40:17 +0500

    Update ImageJRectExample.java
    Нарушение принципов SOLID. Принцип открытости/закрытости.
    Все программные сущности (классы, компоненты, модули, сущности) должны быть открыты для расширения, но закрыты для изменения

--- image-processing/src/main/java/com/baeldung/imageprocessing/imagej/ImageJRectExample.java
+++ image-processing/src/main/java/com/baeldung/imageprocessing/imagej/ImageJRectExample.java
@@ -6,7 +6,7 @@ import ij.process.ImageProcessor;
 
 import java.awt.*;
 
-public class ImageJRectExample {
+public class Rect {
     public static void main(String[] args) {
         ImagePlus imp = IJ.openImage(ImageJRectExample.class.getClassLoader().getResource("lena.jpg").getPath());
         drawRect(imp);
@@ -14,9 +14,14 @@ public class ImageJRectExample {
     }
 
     private static void drawRect(ImagePlus imp) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextLine();
+        int y = in.nextLine();
+        int width = in.nextLine();
+        int height = in.nextLine();
         ImageProcessor ip = imp.getProcessor();
         ip.setColor(Color.BLUE);
         ip.setLineWidth(4);
-        ip.drawRect(10, 10, imp.getWidth() - 20, imp.getHeight() - 20);
+        ip.drawRect(x, y, imp.getWidth() - width, imp.getHeight() - height);
     }
 }
