b23728d00e1f7dbf3917f8caa7b4dc190b13dcca
ArinaYuhimenko <115879035+ArinaYuhimenko@users.noreply.github.com>
2023-05-23 22:01:56 +0500

    Update CarMessageService.java
    Аналогично, добавлен ненужный контекст

--- struts2/src/com/baeldung/struts/CarMessageService.java
+++ struts2/src/com/baeldung/struts/CarMessageService.java
@@ -2,11 +2,11 @@ package com.baeldung.struts;
 
 public class CarMessageService {
 
-    public String getMessage(String carName) {
-        System.out.println("inside getMessage()" + carName);
-        if (carName.equalsIgnoreCase("ferrari"))
+    public String getMessage(String Name) {
+        System.out.println("inside getMessage()" + Name);
+        if (Name.equalsIgnoreCase("ferrari"))
             return "Ferrari Fan!";
-        else if (carName.equalsIgnoreCase("bmw"))
+        else if (Name.equalsIgnoreCase("bmw"))
             return "BMW Fan!";
         else
             return "please choose ferrari Or bmw";
